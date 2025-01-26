42707f8a8c34e818a8c8892afd80f85b1b7ddd03
Claudia Reyes Saez <99850356+claudiarreyes@users.noreply.github.com>
2025-01-26 16:57:56 +1100

    Update download_lcs.py

--- scripts/download_lcs.py
+++ scripts/download_lcs.py
@@ -1,9 +1,56 @@
+import lightkurve as lk
 from astropy.table import Table
-from astropy.io import fits
+from astropy.io import ascii, fits
+import os
 import numpy as numpy
 
-base_dir = '/Users/creyes/Projects/lcs
-df = pd.read_pickle('/Users/creyes/Projects/harps/tables.pkl')
+base_dir = '/Users/creyes/Projects/tables
+
+
+
+
+df['coord_string'] = [ "{:.8f}".format(a) + ' ' + "{:.8f}".format(b) for a,b in zip(df['alpha'], df['delta'])]
+
+for _, coords in df.iterrows():
+    mytable = lk.search_lightcurve(coords['coord_string']).table
+    file_path = os.path.join(base_dir, '{}.ecsv'.format(coords['target_name']))
+    mytable.write(file_path, overwrite=True)
+
+
+mission_list, nobs = [], []
+for string in df.tel_object:
+    try:
+        t = Table.read(basedir +'/{}.ecsv'.format(string))
+        mission_list.append([a for a in set(np.array(t['project']))])
+        nobs.append(len(t))
+    except FileNotFoundError:
+        mission_list.append('')
+        nobs.append(0)
+        
+df['mission'] = mission_list
+df['number_of_lightcurves'] = nobs
+
+# Extract all unique mission names from the 'mission' column
+all_missions = set(mission for missions in df['mission'] for mission in missions)
+
+# Create a new column for each unique mission ands sets boolean if LC available from that mission
+for mission in all_missions:
+    df[mission] = df['mission'].apply(lambda x: mission in x)
+
+tables = []
+for telobj in df.target_name:
+    df[df.number_of_lightcurves>0].copy()
+    try:
+        name = '{}'.format(telobj).replace(' ', '_')
+        t = Table.read('/Users/creyes/Projects/harps/tables/{}.ecsv'.format(name))
+        tables.append(t)
+    except FileNotFoundError:
+        tables.append('No table')
+df['tables'] = tables
+
+
+# df = pd.read_pickle('/Users/creyes/Projects/harps/tables.pkl')
+
 
 
 for k in range(len(df)):
@@ -23,5 +70,5 @@ for k in range(len(df)):
                     tess_bjds = hdulist[1].data['TIME']
                     sap_fluxes = hdulist[1].data['SAP_FLUX']
                     pdcsap_fluxes = hdulist[1].data['PDCSAP_FLUX']
-                fname = base_dir + '/lightcurves/{:05.0f}_HARPS_target_{}_TESS_targetname_{}_TESS_exptime_{}_TESS_mission_{}_TESS_author_{}.txt'.format(k, tel_object, target_name, exptime, mission, author)
+                fname = base_dir + '/lightcurves/{:05.0f}_{}_target_{}_MAST_targetname_{}_MAST_exptime_{}_MAST_mission_{}_MAST_author_{}.txt'.format(project_name, k, tel_object, target_name, exptime, mission, author)
                 np.savetxt(fname, np.column_stack([tess_bjds, sap_fluxes, pdcsap_fluxes]))
